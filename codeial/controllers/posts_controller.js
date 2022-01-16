b1c2641e80cf530f6b137208fe5989666f3d1015
ashishugi <ashishkumarguptacse@gmail.com>
2022-01-16 22:57:08 +0530

    added post correct code

--- codeial/controllers/posts_controller.js
+++ codeial/controllers/posts_controller.js
@@ -1,6 +1,17 @@
 const Post = require("../models/post");
 
 module.exports.posts = function (req, res) {
+  Post.create(
+    {
+      content: req.body.content,
+      user: req.user._id,
+    },
+    function (err, post) {
+      if (err) {
+        console.log("err in creating form");
+      }
+    }
+  );
   res.status(200).json({
     success: true,
     message: "@ the post page",
